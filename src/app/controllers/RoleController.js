66a80ce431a8cc5dc2d26f94069e7dfae87ac078
Robson Maia <robsonmvieira@gmail.com>
2020-02-13 00:10:36 -0300

    :hammer: refactory in index methods

--- src/app/controllers/RoleController.js
+++ src/app/controllers/RoleController.js
@@ -1,4 +1,5 @@
 import Role from '../models/Role'
+import User from '../models/User'
 
 class RoleController {
   async store(req, res) {
@@ -8,7 +9,9 @@ class RoleController {
   }
 
   async index(req, res) {
-    const roles = await Role.findAll()
+    const roles = await Role.findAll({
+      include: [{ model: User, as: 'users', through: { attributes: [] } }]
+    })
     return res.status(200).json(roles)
   }
 }
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,24 +1,26 @@
 import User from '../models/User'
+import Roles from '../models/Role'
 
 class UserController {
   async store(req, res) {
     const data = req.body
     const user = await User.create(data)
+    // await user.setRoles(1)
+    // await user.save()
     return res.status(201).json(user)
   }
 
   async index(req, res) {
-    const users = await User.findAll()
+    const users = await User.findAll({
+      include: [{ model: Roles, as: 'roles', through: { attributes: [] } }]
+    })
     return res.status(200).json(users)
   }
 
   async one(req, res) {
     const { id } = req.params
-    const user = await User.findByPk(id)
-    user.roles.push(2)
-    await user.save()
-    console.log(typeof user.roles)
-    return res.status(200).json(typeof user.roles)
+    const user = await User.findOne({ where: { id }, include: Roles })
+    return res.status(200).json(user)
   }
 }
 
