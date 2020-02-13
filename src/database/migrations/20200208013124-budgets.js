f47092efa8aca8d3f3fd366a725ac9546808f6c9
Robson Maia <robsonmvieira@gmail.com>
2020-02-13 13:37:47 -0300

    :sparkles: create budget migrations fielsd

--- src/database/migrations/20200208013124-budgets.js
+++ src/database/migrations/20200208013124-budgets.js
@@ -1,6 +1,44 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('budgets', { id: Sequelize.INTEGER })
+    return queryInterface.createTable('budgets', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, notNullable: true },
+      license_plate: {
+        type: Sequelize.STRING,
+        notNullable: true,
+        unique: true
+      },
+      mechanical_id: {
+        type: Sequelize.INTEGER,
+        onDelete: 'set null',
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      doBy: {
+        type: Sequelize.INTEGER,
+        onDelete: 'set null',
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      }
+    })
   },
 
   down: queryInterface => {
