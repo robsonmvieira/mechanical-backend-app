8d123d4fb1caeaf46edbc733869a6d019f9f9a2c
Robson Maia <robsonmvieira@gmail.com>
2020-03-08 10:27:43 -0300

    :sparkles: add login route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express'
 import Role from './app/controllers/RoleController'
 import Permission from './app/controllers/PermissionController'
 import UserController from './app/controllers/UserController'
+import BudgetController from './app/controllers/BudgetsController'
+import SessionController from './app/controllers/SessionController'
 
 const routes = new Router()
 
@@ -12,5 +14,8 @@ routes.get('/api/permissions', Permission.index)
 routes.post('/api/permissions', Permission.store)
 routes.get('/api/roles', Role.index)
 routes.post('/api/roles', Role.store)
+routes.get('/api/budgets', BudgetController.index)
+routes.post('/api/budgets', BudgetController.store)
+routes.post('/api/login', SessionController.login)
 
 export default routes
